41fe7587ed11c253684d1636b50ea322ff3db64d
Mark Khor <mark@360.my>
2021-04-09 09:54:37 +0800

    V2.0 Oauth2

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 use bulk360\client;
-require_once "./src/client.php";
+require_once "./vendor/autoload.php";
 
 // Please whitelist your IP and enable API in sms.360.my before you call this 
 
-$smsClient = new client('YOUR_EMAIL_ADD', 'YOUR_PASSWOR');
+$smsClient = new client('mxkazYeJ0P', 'WnPXG7TnDzmzlXEYMSvzMkB3RwzqAbt9MKh91rPb');
 $response = $smsClient->send([
+				'from'	=> '68068',
 				'to'	=> '60123240066',
-				'text'	=> 'Hi from Package 1.3'
+				'text'	=> 'Hi from Package 2.0'
 			]);
-$responseDetails = $smsClient->handleResponse($response);
-print_r($responseDetails);
+print_r($response);
+echo "\n";
 
-$status = $smsClient->status;
-$refID = $smsClient->refID;
+$json_response = json_decode($response);
+print_r($json_response);
+
+$arr_response = json_decode($response, true);
+print_r($arr_response);
 
--- src/client.php
+++ src/client.php
@@ -1,65 +1,111 @@
 <?php
 
-namespace bulk360; 
+namespace bulk360;
+
+use Carbon\Carbon;
 
 class client {
+	use iniFunctions;
+
 	protected $username;
 	protected $password;
-	protected $url = 'https://sms.360.my/gw/bulk360/v1.3';
+	protected $gateway_url = 'http://localhost:81/api/bulk360/v2.0';
 
-	public $refID;
-	public $status;
+	protected $ini_path = "data/token.ini";
+	protected $token_url = "http://localhost:81/oauth/token";
+	protected $access_token;
 
-	public function __construct($username, $password) {
-		$this->username = $username;
-		$this->password = $password;
-		$this->url = $this->url . "?user=$this->username&pass=$this->password&type=$this->type&from=$this->from&servid=$this->servid";
+	public function __construct($username, $password) 
+	{
+		$this->username = $username; 
+		$this->password = $password; 		
 	}
 
-	public function send($message) {
-		$opts = array(
-			"ssl"	=> array(
-		        "verify_peer"		=> 	false,
-		        "verify_peer_name"	=> 	false,
-		    ),
-			'http'	=> array(
-				'method'			=> 	"GET",
-				'header'			=> 	"Accept-language: en\r\n" .
-			            			 	"Cookie: foo=bar\r\n"
-			)
-        );
-        $to = $message['to'];
-        $text = $message['text'];
-		$this->url = $this->url . "&to=".$to."&text=".rawurlencode($text);
+	public function send($sms_data) {
+		$this->prepareAccessToken();
+		$query_string = http_build_query($sms_data);
+		
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $this->url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt_array($ch, [
+			CURLOPT_URL => $this->gateway_url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $query_string,
+			CURLOPT_HTTPHEADER => [
+				"Authorization: Bearer " . $this->access_token,
+				"Accept: application/json"
+			],
+		]);
+
 		$sentResult = curl_exec($ch);
 		if ($sentResult == FALSE) {
-			return 'Curl failed for sending sms to bulk360.. '.curl_error($ch);
+			return '{"code":500,"desc":"Server error, please contact support@360.my... ' . curl_error($ch) . '"}';
 		}
 		curl_close($ch);
 
 		return $sentResult;
 	}
 
-	public function handleResponse($response) {
-		if (strstr($response, 'Curl failed')) {
-			$this->status = 400; 
-			return 'Error';
+	public function prepareAccessToken()
+	{
+		// load token info 
+		$config = $this->loadINI();
+		if ($config['expire_time']) {
+			// subsequent load, check token expiry
+			$recorded_time = new Carbon($config['expire_time']);
+			if ($recorded_time->gte(Carbon::now())) {
+				// token not expire, reuse it 
+				$this->access_token = $config['access_token'];
+				return;
+			}
 		}
 
-		$responseDetails = explode(",", $response);
-		if (count($responseDetails) == 3) {
-			$this->status = $responseDetails[0];
-			$this->refID = $responseDetails[2];
-		}
-		else 
-			$this->status = $response;
-		return $responseDetails;
+		// 1st time load, or token expired, request token
+		$token_data = $this->requestToken($this->username, $this->password);
+
+		$data = [
+			"expire_time"	=> Carbon::now()->addSeconds($token_data['expires_in'])->toString(),	// token expire date time 
+			"access_token"	=> $token_data['access_token']
+		];
+		$this->updateINI($data);
+		$this->access_token = $token_data['access_token'];
 	}
 
+	public function requestToken($username, $password)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $this->token_url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "grant_type=client_credentials&client_id=$username&client_secret=$password",
+			CURLOPT_HTTPHEADER => [
+			  "Accept: application/json"
+			],
+		]);
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+
+		if ($err) {
+			echo "cURL Error #:" . $err;
+			exit();
+		}
+
+		// {"token_type":"Bearer","expires_in":3591,"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxNCIsImp0aSI6IjYxN2ZlNjc3MzE2ZDAyZWYxMmE2NTYxODkxZDllZmY2ZTI4ZTAwM2ZiMjQyMzIyYjgwZjg3M2JlODg3YmVhOTgwMTc3ZTdkODAwN2UyMWQ0IiwiaWF0IjoxNjE3OTAyMTAzLjU2MDg1OCwibmJmIjoxNjE3OTAyMTAzLjU2MDg2NiwiZXhwIjoxNjE3OTA1NzAzLjM5NzU1MSwic3ViIjoiIiwic2NvcGVzIjpbIioiXX0.a73ZYlB2FVxDPqtRhjm_PmDGkgduD81LmB3I28CUa3Oc0fhXxFqAKNiFFAsOnhCipAWVtA_AYR-GE7vN9tUjCzDvDFL7y2dQSgGPzZIowxoMV6idkg9PvVFo9dUZATrZfL550oaxMBTZyHgpkKpmD5mxGKS5XuVcvHN8420IRc7kL365ifsQdoQXbNTTQA17RbnUUFOEK14I4Bei6NhR_9Y6Yn7Pgb9tzauoH-UF0HraCq49beOtB3OeX991RsGa2YFONyA1AmAdaiN3fHnFK_K3RI3NI9ADGQhD3_Q61-LnG4LURmNGylUwOYWWlib0MWM7gD_lyDhUOVa0tU3N3OAUhXMQJfytjugjrninm6qqCiGjpBvaWM380jKqZLcXd1GPqVTY8pG9dYZY8fg37TsZ4MvMqLCVO0boBkUERYwZnHS8uhAnuh1Zmzjh5kqMnLO4c1EFWQrFmviPiLntEsovNd8_AdTfU25yzXbgV12AsxSjEaZxAQhPLwcpzeZy_C3dxAaZCxcKD7IdCWitEZ3G_vD1VHW1aZ401nLwSqmpC9LCJJ6swf_34muKIB8LPRd9XIWpXaUGaJe_qLz_7d4feKZjuAaBvPaay9GTsjGci9HH1rU-mncMGE7fcTrAjVgOlmHi0mL5jpE10AESNSslemRRTmnxCHZLLKQ19w4"}
+
+
+		$Aresponse = json_decode($response, true);
+		// print_r($Aresponse['access_token']);
+		return $Aresponse;
+	}
 }
